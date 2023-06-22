29ec3ecd8f54acfdeae5f19e87b7be2222c0dfb2
paul3083 <paul30833083@gmail.com>
2023-06-23 02:22:41 +0900

    fragment

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,11 +3,10 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    val binding: ActivityMainBinding by lazy {
+    private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
@@ -19,34 +18,34 @@ class MainActivity : AppCompatActivity() {
             setOnNavigationItemSelectedListener {
                 when (it.itemId) {
                     R.id.menu_main_home -> {
-                        val Fragment1 = Fragment1()
+                        Fragment1()
                         supportFragmentManager.beginTransaction()
-                            .replace(R.id.fl_con, Fragment1).commit()
+                            .replace(R.id.fl_con, Fragment1()).commit()
                     }
                     R.id.menu_main_profile-> {
-                        val Fragment2 = Fragment2()
+                        Fragment2()
                         supportFragmentManager.beginTransaction()
-                            .replace(R.id.fl_con, Fragment2).commit()
+                            .replace(R.id.fl_con, Fragment2()).commit()
                     }
                     R.id.menu_main_chat-> {
-                        val Fragment3 = Fragment3()
+                        Fragment3()
                         supportFragmentManager.beginTransaction()
                             .replace(R.id.fl_con, Fragment3()).commit()
                     }
                     R.id.menu_main_analyze-> {
-                        val Fragment4 = Fragment4()
+                        Fragment4()
                         supportFragmentManager.beginTransaction()
                             .replace(R.id.fl_con, Fragment4()).commit()
                     }
                     R.id.menu_main_setting-> {
-                        val Fragment5 = Fragment5()
+                        Fragment5()
                         supportFragmentManager.beginTransaction()
                             .replace(R.id.fl_con, Fragment5()).commit()
                     }
                 }
                 true
             }
-            var selectedItemId = R.id.menu_main_home
+            R.id.menu_main_home
         }
     }
 }
